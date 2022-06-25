133dcda5149b91e3fc80903192eedb322862460b
Sukh <sunnyashiin@gmail.com>
2022-06-25 02:21:44 -0400

    make updates

--- pages/_stores/[store]/index.tsx
+++ pages/_stores/[store]/index.tsx
@@ -16,11 +16,61 @@ import {
   AccordionPanel,
   HStack,
   SimpleGrid,
+  Checkbox,
+  CheckboxGroup,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import image from "next/image";
 import React, { FunctionComponent } from "react";
 import { getStore } from "../../api/store/[storeId]";
+
+interface CategoryBrowserProps {}
+
+const CategoryBrowser: FunctionComponent<CategoryBrowserProps> = () => {
+  return (
+    <Accordion w="full" defaultIndex={[0]} allowMultiple>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 1 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <CheckboxGroup
+            colorScheme="green"
+            defaultValue={["naruto", "kakashi"]}
+          >
+            <VStack spacing={2} align="flex-start" ml={4}>
+              <Checkbox value="naruto">Naruto</Checkbox>
+              <Checkbox value="sasuke">Sasuke</Checkbox>
+              <Checkbox value="kakashi">Kakashi</Checkbox>
+            </VStack>
+          </CheckboxGroup>
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 2 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+};
 
 interface SiteIndexProps {}
 
@@ -46,9 +96,9 @@ const SiteIndex: FunctionComponent<SiteIndexProps> = ({
         </Center>
 
         <HStack spacing={10} align="flex-start" w="full">
-          {/* <Box flex="1" w="full">
+          <Box flex="1" w="full">
             <CategoryBrowser />
-          </Box> */}
+          </Box>
           <SimpleGrid minChildWidth={200} spacing={10} flex="2">
             {products.map((product: any) => {
               return (
